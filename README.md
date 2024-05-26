be8f80e7e710f0a608534a40dc164e82a743f5d7
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2024-05-26 21:39:15 +0530

    pushing to publish the app

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Insurance Premium Prediction
+---
+title: Insurance Premium Prediction
+emoji: 🐳
+colorFrom: purple
+colorTo: gray
+sdk: docker
+app_port: 8501
+---
+
+### Clone the Repository
+```
+https://github.com/nikhil-xyz/ipp.git
+```
+### Create a Conda environment and activating it
+```
+conda create -n venv python=3.8 -y
+conda activate venv
+```
+
+### Execute the command
+```
+streamlit run app.py
+```
+
+### Streamlit Exposed port
+```
+8501
+```
+
+###  Setup github secrets:
+
+    HF_TOKEN =
--- app.py
+++ app.py
@@ -1,89 +1,102 @@
 import streamlit as st
 import pandas as pd
 from insurancePP.pipeline.prediction import PredictionPipeline
+import asyncio
+from insurancePP.logging import logger
+
 
 st.title('Insurance Premium Prediction')
-def callback_search():
-    #   Destroying old sessions
-    st.session_state['search_btn'] = False
 
-# Extracting features from the webpage
-# capturing the selected age
-option_age = st.selectbox(
-        'select the age',
-        [i for i in range(15, 65)],
-        on_change=callback_search
-    )
-# print(option_age)
-
-# capturing the selected gender
-option_gender = st.selectbox(
-        'select the gender',
-        ['female', 'male'],
-        on_change=callback_search
-    )
-# print(option_gender)
-
-# capturing the bmi
-# option_bmi = st.text_input(
-#         'enter the bmi (values should be in range 15-60)',
-#         on_change=callback_search
-#     )
-
-option_bmi = st.text_input(
-        'enter the bmi (values should be in range 15-60)',
-        on_change=callback_search
-    )
-
-
-# capturing the number of children
-option_children = st.selectbox(
-        'select number of children',
-        [i for i in range(6)],
-        on_change=callback_search
-    )
-# print(option_children)
-
-# capturing the smoker
-option_smoker = st.selectbox(
-        'is the candidate smoker',
-        ['yes', 'no'],
-        on_change=callback_search
-    )
-# print(option_smoker)
-
-# capturing the region
-option_region = st.selectbox(
-        'select the region',
-        ['southwest', 'southeast', 'northwest', 'northeast'],
-        on_change=callback_search
-    )
-
-
-custom_data = {
-    'age': [option_age],
-    'sex': [option_gender],
-    'bmi': [option_bmi],
-    'children': [option_children],
-    'smoker': [option_smoker],
-    'region': [option_region]
-}
+async def async_task():
+    # executing main.py
+    try:
+        os.system("python main.py")
+        logger.info("Training Successful!")
+    except Exception as e:
+        logger.info(f"Error occured {e}")
 
 
 
-prediction = st.button('Predict')
-# adding session state fot 'prediction' button
-if st.session_state.get('search_btn') != True:
-    st.session_state['search_btn'] = prediction
+async def main():
+    await async_task()
 
-# prediction and printing the output
-if st.session_state['search_btn']:
-    data_df = pd.DataFrame(custom_data)
-    data_df['bmi'] = data_df['bmi'].astype(float)
 
-    print(data_df.dtypes)
-    pipe = PredictionPipeline()
-    result = pipe.predicting_result(data_df)
-    st.write('The Predicted Expense is: ')
-    st.write(result)
+    def callback_search():
+        #   Destroying old sessions
+        st.session_state['search_btn'] = False
+
+    # Extracting features from the webpage
+    # capturing the selected age
+    option_age = st.selectbox(
+            'select the age',
+            [i for i in range(15, 65)],
+            on_change=callback_search
+        )
+    # print(option_age)
+
+    # capturing the selected gender
+    option_gender = st.selectbox(
+            'select the gender',
+            ['female', 'male'],
+            on_change=callback_search
+        )
+
+    option_bmi = st.text_input(
+            'enter the bmi (values should be in range 15-60)',
+            on_change=callback_search
+        )
+
+
+    # capturing the number of children
+    option_children = st.selectbox(
+            'select number of children',
+            [i for i in range(6)],
+            on_change=callback_search
+        )
+    # print(option_children)
+
+    # capturing the smoker
+    option_smoker = st.selectbox(
+            'is the candidate smoker',
+            ['yes', 'no'],
+            on_change=callback_search
+        )
+    # print(option_smoker)
+
+    # capturing the region
+    option_region = st.selectbox(
+            'select the region',
+            ['southwest', 'southeast', 'northwest', 'northeast'],
+            on_change=callback_search
+        )
+
+
+    custom_data = {
+        'age': [option_age],
+        'sex': [option_gender],
+        'bmi': [option_bmi],
+        'children': [option_children],
+        'smoker': [option_smoker],
+        'region': [option_region]
+    }
+
+
+
+    prediction = st.button('Predict')
+    # adding session state fot 'prediction' button
+    if st.session_state.get('search_btn') != True:
+        st.session_state['search_btn'] = prediction
+
+    # prediction and printing the output
+    if st.session_state['search_btn']:
+        data_df = pd.DataFrame(custom_data)
+        data_df['bmi'] = data_df['bmi'].astype(float)
+
+        # print(data_df.dtypes)
+        pipe = PredictionPipeline()
+        result = pipe.predicting_result(data_df)
+        st.write('The Predicted Expense is: ')
+        st.write(result)
+
+asyncio.run(main())
 
